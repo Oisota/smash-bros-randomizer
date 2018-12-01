fc84f5440c42c9a36f23023478735183bf8470d0
Derek Morey <derek.o.morey@gmail.com>
2018-11-30 20:08:44 -0500

    added 4 players randomization

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ const App = new Vue({
 		damage: '',
 		items: '',
 		lives: '',
-		character: '',
+		player1: '',
+		player2: '',
+		player3: '',
+		player4: '',
 		damageOptions: [50, 100, 200],
-		itemOptions: ['none', 'very low', 'low', 'middle', 'high', 'very high'],
+		itemOptions: [
+			'none', 'none', 'none', 'none', 'none',
+			'very low', 'low', 'middle', 'high', 'very high'
+		],
 		liveOptions: ['2 min', '3 stock'],
 		characterOptions: [
 			'Luigi', 'Mario', 'Donkey Kong', 'Link', 'Samus', 'Captain Falcon',
@@ -26,12 +32,18 @@ const App = new Vue({
 			const damageResult = this.randomInt(0, this.damageOptions.length);
 			const itemResult = this.randomInt(0, this.itemOptions.length);
 			const liveResult = this.randomInt(0, this.liveOptions.length);
-			const characterResult = this.randomInt(0, this.characterOptions.length);
+			const player1result = this.randomInt(0, this.characterOptions.length);
+			const player2result = this.randomInt(0, this.characterOptions.length);
+			const player3result = this.randomInt(0, this.characterOptions.length);
+			const player4result = this.randomInt(0, this.characterOptions.length);
 
 			this.damage = this.damageOptions[damageResult];
 			this.items = this.itemOptions[itemResult];
 			this.lives = this.liveOptions[liveResult];
-			this.character = this.characterOptions[characterResult];
+			this.player1 = this.characterOptions[player1result];
+			this.player2 = this.characterOptions[player2result];
+			this.player3 = this.characterOptions[player3result];
+			this.player4 = this.characterOptions[player4result];
 		}
 	}
 });
